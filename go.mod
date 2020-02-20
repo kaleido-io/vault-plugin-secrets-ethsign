b7881d4bbf30cac91eb83f93349f3d85d0fd94ea
Jim Zhang <jim.zhang@kaleido.io>
2020-02-20 17:22:03 -0500

    Another version of the plugin that can successfully deploy as a plugin

--- go.mod
+++ go.mod
@@ -4,9 +4,13 @@ go 1.13
 
 require (
 	github.com/ethereum/go-ethereum v1.9.11
+	github.com/hashicorp/errwrap v1.0.0
 	github.com/hashicorp/go-hclog v0.8.0
 	github.com/hashicorp/vault/api v1.0.4
 	github.com/hashicorp/vault/sdk v0.1.13
+	github.com/miguelmota/go-coinmarketcap v0.1.6
+	github.com/pborman/uuid v0.0.0-20170112150404-1b00554d8222
+	github.com/sethvargo/go-diceware v0.2.0
 	github.com/shopspring/decimal v0.0.0-20200105231215-408a2507e114
 	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2
 )
--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// Copyright © 2020 Kaleido
+// Copyright © 2018 Immutability, LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -15,30 +15,27 @@
 package main
 
 import (
-  "os"
+	"log"
+	"os"
 
-  "github.com/hashicorp/go-hclog"
-  "github.com/hashicorp/vault/api"
-  "github.com/hashicorp/vault/sdk/plugin"
-  "github.com/kaleido-io/eth-hsm/backend"
+	"github.com/hashicorp/vault/api"
+	"github.com/hashicorp/vault/sdk/plugin"
 )
 
 func main() {
-  pluginMeta := &api.PluginAPIClientMeta{}
-  flags := pluginMeta.FlagSet()
-  flags.Parse(os.Args[1:])
+	apiClientMeta := &api.PluginAPIClientMeta{}
+	flags := apiClientMeta.FlagSet()
+	flags.Parse(os.Args[1:]) // Ignore command, strictly parse flags
 
-  tlsConfig := pluginMeta.GetTLSConfig()
-  tlsProviderFunc := api.VaultPluginTLSProvider(tlsConfig)
+	tlsConfig := apiClientMeta.GetTLSConfig()
+	tlsProviderFunc := api.VaultPluginTLSProvider(tlsConfig)
 
-  err := plugin.Serve(&plugin.ServeOpts{
-    BackendFactoryFunc: backend.Factory,
-    TLSProviderFunc:    tlsProviderFunc,
-  })
-  if err != nil {
-    logger := hclog.New(&hclog.LoggerOptions{})
-
-    logger.Error("Eth-HSM plugin failed during startup, shutting down.", "error", err)
-    os.Exit(1)
-  }
+	err := plugin.Serve(&plugin.ServeOpts{
+		BackendFactoryFunc: Factory,
+		TLSProviderFunc:    tlsProviderFunc,
+	})
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
 }
